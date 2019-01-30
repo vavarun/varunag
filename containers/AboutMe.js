7c412fe51beca19be9053688db769fc9d16f5a3f
Varun A <varunag@outlook.com>
2019-01-30 02:45:46 +0100

    feat: add skills and icons

--- containers/AboutMe.js
+++ containers/AboutMe.js
@@ -15,12 +15,30 @@ const Title = styled.h1`
   text-transform: uppercase;
 `
 
+const Span = styled.span`
+  font-family: Roboto Condensed;
+  font-weight: 300;
+  line-height: 23px;
+  font-size: 16px;
+  font-style: normal;
+`
+
 function AboutMe() {
   return (
     <Header>
       <RevealingText color={'#fff'}>
         <Title>About Me</Title>
       </RevealingText>
+      <Span>
+        I am a multidisciplinary engineer specialized in JavaScript, React,
+        Nodejs, client- server architecture, algorithms, and data modelling.
+        Strong problem solver with a passion for making things and
+        entrepreneurial ventures. I have worked across multiple countries,
+        developing good client management, communication and organizational
+        skills along the way. My progressive engineering experience has allowed
+        me to work in business development, web technology, engineering
+        modelling and real-estate development.
+      </Span>
     </Header>
   )
 }
--- containers/Skills.js
+++ containers/Skills.js
@@ -6,7 +6,7 @@ const Header = styled.header`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: center;
+  margin-right: 125px;
 `
 const Title = styled.h1`
   font-family: Playfair Display;
@@ -16,12 +16,65 @@ const Title = styled.h1`
   text-transform: uppercase;
 `
 
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 45px;
+`
+const Skill = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-family: Roboto Condensed;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  font-size: 16px;
+  align-items: center;
+  margin-bottom: 30px;
+`
+
+const Img = styled.img`
+  object-fit: cover;
+  height: 50px;
+  margin-right: 45px;
+`
+
 function Skills() {
   return (
     <Header>
       <RevealingText color={'#fff'}>
         <Title>Skills</Title>
       </RevealingText>
+      <Column>
+        <Skill>
+          <Img src="/static/react-icon.svg" />
+          React
+        </Skill>
+        <Skill>
+          <Img src="/static/nodejs-icon.svg" />
+          Nodejs
+        </Skill>
+        <Skill>
+          <Img src="/static/react-icon.svg" />
+          Javascript
+        </Skill>
+        <Skill>
+          <Img src="/static/mongodb-icon.svg" />
+          MongoDB
+        </Skill>
+        <Skill>
+          <Img src="/static/elasticsearch-icon.svg" />
+          ElasticSearch
+        </Skill>
+        <Skill>
+          <Img src="/static/react-icon.svg" />
+          GraphQL
+        </Skill>
+        <Skill>
+          <Img src="/static/react-icon.svg" />
+          Next.js
+        </Skill>
+      </Column>
     </Header>
   )
 }
